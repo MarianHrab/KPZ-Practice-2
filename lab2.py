3e5c75cdcd730d2c64ed6ee2b75ae5db0aa772ec
MarianHrab <99960652+MarianHrab@users.noreply.github.com>
2022-03-25 12:41:06 +0300

    lab2

--- lab2.py
+++ lab2.py
@@ -1,8 +1,14 @@
 def prime_num_generator():
-    for n in range(2, 100):  # n starts from 2 to end
+    for n in range(2, 102):  # n starts from 2 to end
         for x in range(2, n):  # check if x can be divided by n
             if n % x == 0:  # if true then n is not prime
                 break
         else:  # if x is found after exhausting all values of x
             yield n
-            n += 1
+        n += 1
+
+
+a = prime_num_generator()
+print(next(a))
+print(next(a))
+
